830873d9de24e741b2e0a183fa5106a510820070
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-05-10 15:00:30 +1200

    questions

--- 05_question_generator_v2.py
+++ 05_question_generator_v2.py
@@ -1,5 +1,5 @@
 """Component 3 (question_generator) v1
-Generates random choice of tokens in random order and asks the question to the user"""
+Generates random choice of tokens in random order and asks a question to the user"""
 
 import random
 
@@ -17,55 +17,55 @@ for item in range(1):
     if token == "tahi":
         print("What number is tahi? ")
     elif token == "rua":
-        print("What number is rua")
+        print("What number is rua? ")
     elif token == "toru":
-        print("What number is toru")
+        print("What number is toru? ")
     elif token == "whā":
-        print("What number is whā")
+        print("What number is whā? ")
     elif token == "rima":
-        print("What number is rima")
+        print("What number is rima? ")
     elif token == "ono":
-        print("What number is ono")
+        print("What number is ono? ")
     elif token == "whitu":
-        print("What number is whitu")
+        print("What number is whitu? ")
     elif token == "waru":
-        print("What number is waru")
+        print("What number is waru? ")
     elif token == "iwa":
-        print("What number is iwa")
+        print("What number is iwa? ")
     elif token == "tekau":
-        print("What number is tekau")
+        print("What number is tekau? ")
     elif token == "tekau mā tahi":
-        print("What number is tekau mā tahi")
+        print("What number is tekau mā tahi? ")
     elif token == "tekau mā rua":
-        print("What number is tekau mā rua")
+        print("What number is tekau mā rua? ")
     elif token == "tekau mā toru":
-        print("What number is tekau mā toru")
+        print("What number is tekau mā toru? ")
     elif token == "tekau mā whā":
-        print("What number is tekau mā whā")
+        print("What number is tekau mā whā? ")
     elif token == "tekau mā rima":
-        print("What number is tekau mā rima")
+        print("What number is tekau mā rima? ")
     elif token == "tekau mā ono":
-        print("What number is tekau mā ono")
+        print("What number is tekau mā ono? ")
     elif token == "tekau mā whitu":
-        print("What number is tekau mā whitu")
+        print("What number is tekau mā whitu? ")
     elif token == "tekau mā waru":
-        print("What number is tekau mā waru")
+        print("What number is tekau mā waru? ")
     elif token == "tekau mā iwa":
-        print("What number is tekau mā iwa")
+        print("What number is tekau mā iwa? ")
     elif token == "rua tekau":
-        print("What number is rua tekau")
+        print("What number is rua tekau? ")
     elif token == "rua tekau mā tahi":
-        print("What number is rua tekau mā tahi")
+        print("What number is rua tekau mā tahi? ")
     elif token == "rua tekau mā rua":
-        print("What number is rua tekau mā rua")
+        print("What number is rua tekau mā rua? ")
     elif token == "rua tekau mā toru":
-        print("What number is rua tekau mā toru")
+        print("What number is rua tekau mā toru? ")
     elif token == "rua tekau mā whā":
-        print("What number is rua tekau mā whā")
+        print("What number is rua tekau mā whā? ")
     elif token == "rua tekau mā rima":
-        print("What number is rua tekau mā rima")
+        print("What number is rua tekau mā rima? ")
     elif token == "rua tekau mā ono":
-        print("What number is rua tekau mā ono")
+        print("What number is rua tekau mā ono? ")
     elif token == "rua tekau mā whitu":
         print("What number is rua tekau mā whitu? ")
     elif token == "rua tekau mā waru":
@@ -74,4 +74,3 @@ for item in range(1):
         print("What number is rua tekau mā iwa? ")
     elif token == "toru tekau":
         print("What number is toru tekau? ")
-
