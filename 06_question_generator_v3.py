8178a02cb27bf070709b196aea97e5a6221caf8f
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-05-29 20:11:05 +1200

    Maori_quiz_final

--- 06_question_generator_v3.py
+++ 06_question_generator_v3.py
@@ -1,41 +1,51 @@
-import random
+# questions
+def quiz(num_questions):
+    score_ = 0
+    num_questions_ = num_questions
+    rounds = 0
 
-numbers = ["tahi", "rua", "toru", "whā", "rima", "ono",
-           "whitu", "waru", "iwa", "tekau", "tekau mā tahi", "tekau mā rua",
-           "tekau mā toru", "tekau mā whā", "tekau mā rima", "tekau mā ono",
-           "tekau mā whitu", "tekau mā waru", "tekau mā iwa", "rua tekau",
-           "rua tekau mā tahi", "rua tekau mā rua", "rua tekau mā toru",
-           "rua tekau mā whā", "rua tekau mā rima", "rua tekau mā ono",
-           "rua tekau mā whitu", "rua tekau mā waru", "rua tekau mā iwa", "toru tekau"]
+    numbers = ["tahi", "rua", "toru", "whā", "rima", "ono",
+               "whitu", "waru", "iwa", "tekau", "tekau mā tahi",
+               "tekau mā rua", "tekau mā toru", "tekau mā whā",
+               "tekau mā rima", "tekau mā ono", "tekau mā whitu",
+               "tekau mā waru", "tekau mā iwa", "rua tekau",
+               "rua tekau mā tahi", "rua tekau mā rua", "rua tekau mā toru",
+               "rua tekau mā whā", "rua tekau mā rima", "rua tekau mā ono",
+               "rua tekau mā whitu", "rua tekau mā waru", "rua tekau mā iwa",
+               "toru tekau"]
 
-questions = ["1", "2", "3", "4", "5", "6",
-             "7", "8", "9", "10", "11", "12",
-             "13", "14", "15", "16",
-             "17", "18", "19", "20",
-             "21", "22", "23",
-             "24", "25", "26",
-             "27", "28", "29", "30"]
+    answers = ["1", "2", "3", "4", "5", "6",
+               "7", "8", "9", "10", "11", "12",
+               "13", "14", "15", "16",
+               "17", "18", "19", "20",
+               "21", "22", "23",
+               "24", "25", "26",
+               "27", "28", "29", "30"]
 
+    while rounds <= num_questions_:
+        rounds += 1
+        question = random.choice(numbers)
+        attempt = input(f"What number is {question}: ")
+        print()
 
-while True:
-    question = random.choice(numbers)
-    attempt = input(f"What number is {question}: ")
+        answer_index = numbers.index(question)
+        answer = answers[answer_index]
 
-    answer_index = numbers.index(question)
-    answer = questions[answer_index]
-    score = 0
-    number_of_questions = balance
-    if attempt == answer:
-        print("Well done, that was correct")
-        numbers.pop(answer_index)
-        questions.pop(answer_index)
-        score += 1
-        number_of_questions -= 1
-    elif attempt == "":
-        print("Invalid please try again")
-    else:
-        print("Unlucky, that was incorrect")
-        numbers.pop(answer_index)
-        questions.pop(answer_index)
-        number_of_questions -= 1
-        print(number_of_questions)
+        if attempt == answer:
+            print()
+            print(formatter("!", "Well done, that was correct"))
+            numbers.pop(answer_index)
+            answers.pop(answer_index)
+            score_ += 1
+            print()
+        elif attempt == "":
+            print()
+            print(formatter("-", "Invalid please try again"))
+            print()
+        else:
+            print()
+            print(formatter("^", "Unlucky, that was incorrect"))
+            numbers.pop(answer_index)
+            answers.pop(answer_index)
+            print()
+    return score_
